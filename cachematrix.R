391f89e8d56af858527a07929c5c0b570203ba21
GeoShr <42970968+GeoShr@users.noreply.github.com>
2018-09-19 20:35:15 -0400

    Assignment 2
    Added in my code for programming assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## makeCacheMatrix: Build a set of functions and return them in a 
+## list to the parent environment.
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  ### initialize the inverse of the matrix to NULL.
+  inv_mtx <- NULL
+  
+  ## Following the example of makeVector(), define the get and set functions.
+  set <- function(arg1) {
+    x <<- arg1
+    inv_mtx <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) inv_mtx <<- solve
+  getsolve <- function() inv_mtx
+  
+  ## Create a list whose elements are the methods defined above.
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
+  
 
-
-## Write a short comment describing this function
-
+## Caclulate and populate, or retrieve, the inverse matrix
+## from an object of type makeCacheMatrix.        
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ## Check to see if a cached inverse of the matrix is available.
+  ## If it is, return it to the parent environment.
+  inv_mtx <- x$getsolve()
+  if(!is.null(inv_mtx)) {
+    message("Returning the cached inverse")
+    return(inv_mtx)
+  }
+  
+  ## If the inverse is not available, get the matrix from the input object,
+  ## run solve to get the inverse of the matrix and return it.
+  data <- x$get()
+  inv_mtx <- solve(data, ...)
+  x$setsolve(inv_mtx)
+  inv_mtx
 }
